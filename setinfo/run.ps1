e99b003f4afdcc3003d9208bce1dc53fe46df569
Julio Rosua Rodriguez <rosuarodriguez@evident.nl>
2020-01-15 15:11:53 +0100

    added new config

--- setinfo/run.ps1
+++ setinfo/run.ps1
@@ -3,7 +3,7 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-$connectionString = "Server=tcp:skyworkz-sqlserver.database.windows.net,1433;Initial Catalog=skyworkz-sql;Persist Security Info=False;User ID=sqladmin;Password=kiloploki323!;MultipleActiveResultSets=true;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+$connectionString = "Server=tcp:sw-exam-sqlserver.database.windows.net,1433;Initial Catalog=sw-exam-database;Persist Security Info=False;User ID=sqladmin;Password=Passwordvalue#;MultipleActiveResultSets=true;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 $query = "INSERT example VALUES (@name, @lastname);"
 
 try 
